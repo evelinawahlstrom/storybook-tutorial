be2f44132467c93dfc54275b87d1984768214bb8
Evelina Wahlstrom <evelina@loveholidayss-MacBook-Pro.local>
2020-03-11 17:27:35 +0000

    started-creating-an-addon

---
+++ .storybook/addons/design-assets.js
@@ -0,0 +1,16 @@
+//.storybook/addons/design-assets.js
+import React from "react";
+import { AddonPanel } from "@storybook/components";
+import { addons, types } from "@storybook/addons";
+
+addons.register("my/design-assets", () => {
+  addons.add("design-assets/panel", {
+    title: "assets",
+    type: types.PANEL,
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        implement
+      </AddonPanel>
+    )
+  });
+});
